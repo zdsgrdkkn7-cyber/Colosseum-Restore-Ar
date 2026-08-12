7c01ae6124bf53e05979d226dcb50db71da41cbc
zdsgrdkkn7-cyber <zdsgrdkkn7@privaterelay.appleid.com>
2026-08-11 22:31:47 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="colosseum-restoration-v1";
+const CACHE="colosseum-restoration-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
