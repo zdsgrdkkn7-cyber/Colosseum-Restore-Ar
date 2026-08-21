b4086c825d2b9e212b4d5946e8bf4684357d23c0
zdsgrdkkn7-cyber <zdsgrdkkn7@privaterelay.appleid.com>
2026-08-20 23:40:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE="colosseum-restoration-v16-3";
+const CORE=[
+  "./","./index.html","./styles.css?v=16.1","./app.js?v=16.1","./pdf.js?v=16.3",
+  "./manifest.webmanifest",
+  "./assets/ho-oh.png","./assets/lugia.png","./assets/celebi.webp","./assets/title-jp.png",
+  "./assets/pokeball-active.svg","./assets/pokeball-inactive.svg"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>
+      Promise.allSettled(CORE.map(url=>cache.add(url)))
+    ).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match(event.request))
+  );
+});
